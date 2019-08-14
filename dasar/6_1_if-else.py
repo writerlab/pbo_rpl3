e8519162ffda2c054222f67bee009f08c1512fa8
writerlab <netspytux@gmail.com>
2019-08-14 18:33:31 +0700

    rename and remove

--- dasar/6_1_if-else.py
+++
@@ -1,6 +0,0 @@
-jawab = input('ibukota indonesia: ')
-
-if jawab.lower() == 'jakarta':
-  print("Jawaban kamu benar!")
-else:
-  print("Yaah.. masa ga tau :(")
--- dasar/6_2_if-elif-else.py
+++
@@ -1,8 +0,0 @@
-x = input('masukkan bilangan 1 s.d 10: ')
-
-if int(x) > 0:
-  print(x,"adalah bilangan positif")
-elif int(x) < 0:
-  print(x,"adalah bilangan negatif")
-else:
-  print(x,"adalah NOL") 
