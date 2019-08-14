4b33b051bcc23c3e52e89aa6b38f45feaf5c1a21
writerlab <netspytux@gmail.com>
2019-08-14 18:33:10 +0700

    rename

---
+++ dasar/6_3_if-elif-else.py
@@ -0,0 +1,8 @@
+x = input('masukkan bilangan 1 s.d 10: ')
+
+if int(x) > 0:
+  print(x,"adalah bilangan positif")
+elif int(x) < 0:
+  print(x,"adalah bilangan negatif")
+else:
+  print(x,"adalah NOL") 
